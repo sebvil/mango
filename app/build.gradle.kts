24e041fb9a855a2c03ba514b244fd62eec8d3f82
sebvil <>
2023-06-27 22:57:54 -0700

    Added NavigationRoutes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@
 plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.org.jetbrains.kotlin.plugin.serialization)
+    alias(libs.plugins.org.jetbrains.kotlin.kapt)
+
 }
 
 android {
@@ -31,17 +34,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.7"
     }
     packaging {
         resources {
@@ -60,6 +63,13 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.kotlin.serialization)
+    implementation(libs.navigation)
+    implementation(libs.hilt.navigation)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.accompanist.navigation.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
@@ -67,4 +77,10 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+}
+
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/sebastianvm/mango/ui/navigation/NavigationRoutes.kt
@@ -0,0 +1,104 @@
+package com.sebastianvm.mango.ui.navigation
+
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.bottomSheet
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+
+const val ARGS = "ARGS"
+
+enum class NavigationRoute(val hasArgs: Boolean)
+
+
+interface NavigationArguments : Parcelable
+
+sealed class NavigationDestination(
+    val navigationRoute: NavigationRoute,
+    open val arguments: NavigationArguments?,
+)
+
+private val module = SerializersModule {
+    polymorphic(NavigationArguments::class) {
+
+    }
+}
+
+private val json = Json { serializersModule = module }
+
+fun NavController.navigateTo(
+    destination: NavigationDestination, builder: NavOptionsBuilder.() -> Unit = {}
+) {
+    val navRoute = if (destination.navigationRoute.hasArgs) {
+        val encodedArgs = Uri.encode(json.encodeToString(destination.arguments))
+        "${destination.navigationRoute.name}/args=$encodedArgs"
+    } else {
+        destination.navigationRoute.name
+    }
+    navigate(navRoute, builder)
+}
+
+
+fun getArgumentsType(): NavType<NavigationArguments> =
+    object : NavType<NavigationArguments>(false) {
+        override fun put(bundle: Bundle, key: String, value: NavigationArguments) {
+            bundle.putParcelable(key, value)
+        }
+
+        override fun get(bundle: Bundle, key: String): NavigationArguments {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                bundle.getParcelable(key, NavigationArguments::class.java)!!
+            } else {
+                @Suppress("deprecation")
+                bundle.getParcelable(key)!!
+            }
+        }
+
+        override fun parseValue(value: String): NavigationArguments {
+            return json.decodeFromString(value)
+        }
+    }
+
+enum class DestinationType { Screen, BottomSheet }
+
+@OptIn(ExperimentalMaterialNavigationApi::class)
+inline fun <reified VM : ViewModel> NavGraphBuilder.screenDestination(
+    destination: NavigationRoute,
+    destinationType: DestinationType,
+    crossinline screen: @Composable (VM) -> Unit
+) {
+    val route = if (destination.hasArgs) {
+        "${destination.name}/args={$ARGS}"
+    } else {
+        destination.name
+    }
+    val args = if (destination.hasArgs) listOf(navArgument(ARGS) {
+        type = getArgumentsType()
+    }) else listOf()
+
+    when (destinationType) {
+        DestinationType.Screen -> composable(route = route, arguments = args) {
+            val screenViewModel = hiltViewModel<VM>()
+            screen(screenViewModel)
+        }
+
+        DestinationType.BottomSheet -> bottomSheet(route = route, arguments = args) {
+            val screenViewModel = hiltViewModel<VM>()
+            screen(screenViewModel)
+        }
+    }
+}
