5387d0966ab3babb19ef30fad66fd629e546385c
sebvil <>
2023-07-17 21:27:49 -0700

    refactored Tax model

--- app/src/main/java/com/sebastianvm/mango/database/models/TaxEntity.kt
+++ app/src/main/java/com/sebastianvm/mango/database/models/TaxEntity.kt
@@ -1,16 +1,15 @@
 package com.sebastianvm.mango.database.models
 
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.sebastianvm.mango.model.Deduction
 import com.sebastianvm.mango.model.Jurisdiction
 import com.sebastianvm.mango.model.Tax
 import com.sebastianvm.mango.model.TaxType
 
-@Entity
+@Entity(primaryKeys = ["name", "year"])
 data class TaxEntity(
-    @PrimaryKey(autoGenerate = true) override val id: Long,
     override val name: String,
+    override val year: Int,
     override val jurisdiction: Jurisdiction,
     override val taxType: TaxType,
     override val deductions: List<Deduction>
