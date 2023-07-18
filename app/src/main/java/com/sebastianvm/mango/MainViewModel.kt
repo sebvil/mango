ac2adc16c6c7d39d0462fa38fd64b31ed7f7d278
sebvil <>
2023-07-17 23:33:08 -0700

    format

--- app/src/main/java/com/sebastianvm/mango/MainViewModel.kt
+++ app/src/main/java/com/sebastianvm/mango/MainViewModel.kt
@@ -8,12 +8,10 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-
 object MainState : State
 
 sealed interface MainUserAction : UserAction
 object InitDatabase : MainUserAction
-
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val taxRepository: TaxRepository) : BaseViewModel<MainState, MainUserAction>(MainState) {
--- app/src/main/java/com/sebastianvm/mango/database/dao/TaxDao.kt
+++ app/src/main/java/com/sebastianvm/mango/database/dao/TaxDao.kt
@@ -1,16 +1,10 @@
 package com.sebastianvm.mango.database.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
 import androidx.room.Upsert
 import com.sebastianvm.fakegen.FakeClass
 import com.sebastianvm.fakegen.FakeCommandMethod
-import com.sebastianvm.fakegen.FakeQueryMethod
-import com.sebastianvm.mango.database.models.IncomeSourceEntity
 import com.sebastianvm.mango.database.models.TaxEntity
-import kotlinx.coroutines.flow.Flow
 
 @FakeClass
 @Dao
@@ -19,5 +13,4 @@ interface TaxDao {
     @FakeCommandMethod
     @Upsert
     suspend fun upsertTaxes(taxes: List<TaxEntity>)
-
 }
