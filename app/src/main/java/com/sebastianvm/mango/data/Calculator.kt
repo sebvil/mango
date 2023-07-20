c356d8adc9402e4c1dcd57a237e959579f7027b0
sebvil <>
2023-07-19 18:55:48 -0700

    paycheck tax

--- app/src/main/java/com/sebastianvm/mango/data/Calculator.kt
+++ app/src/main/java/com/sebastianvm/mango/data/Calculator.kt
@@ -2,45 +2,101 @@ package com.sebastianvm.mango.data
 
 import com.sebastianvm.mango.model.Fixed
 import com.sebastianvm.mango.model.FixedWithMax
+import com.sebastianvm.mango.model.PayFrequency
 import com.sebastianvm.mango.model.Progressive
 import com.sebastianvm.mango.model.Tax
+import com.sebastianvm.mango.util.coroutines.DefaultDispatcher
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 interface Calculator {
 
     suspend fun calculateTax(grossIncome: BigDecimal, tax: Tax): BigDecimal
+    suspend fun calculatePaycheckTax(
+        grossIncome: BigDecimal,
+        paidYearToDate: BigDecimal,
+        tax: Tax,
+        frequency: PayFrequency
+    ): BigDecimal
+
 }
 
-class CalculatorImpl : Calculator {
+class CalculatorImpl(@DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher) :
+    Calculator {
 
     override suspend fun calculateTax(grossIncome: BigDecimal, tax: Tax): BigDecimal {
-        val incomeWithDeductions =
-            grossIncome - tax.deductions.fold(BigDecimal(0)) { acc, deduction ->
-                acc + deduction.amount
-            }
-        return when (val taxType = tax.taxType) {
-            is Fixed -> incomeWithDeductions * taxType.rate
-            is FixedWithMax -> minOf(incomeWithDeductions, taxType.maxTaxableIncome) * taxType.rate
-            is Progressive -> {
-                taxType.taxBrackets.foldIndexed(BigDecimal(0)) { index, acc, taxBracket ->
-                    val minIncome =
-                        taxType.taxBrackets.getOrNull(index - 1)?.maxIncome ?: BigDecimal(0)
-                    if (minIncome > incomeWithDeductions) {
-                        acc
-                    } else {
-                        val maxIncome =
-                            incomeWithDeductions.min(taxBracket.maxIncome ?: incomeWithDeductions)
-                        acc + (maxIncome - minIncome) * taxBracket.rate
+        return withContext(defaultDispatcher) {
+            val incomeWithDeductions =
+                grossIncome - tax.deductions.fold(BigDecimal(0)) { acc, deduction ->
+                    acc + deduction.amount
+                }
+            when (val taxType = tax.taxType) {
+                is Fixed -> incomeWithDeductions * taxType.rate
+                is FixedWithMax -> minOf(
+                    incomeWithDeductions,
+                    taxType.maxTaxableIncome
+                ) * taxType.rate
+
+                is Progressive -> {
+                    taxType.taxBrackets.foldIndexed(BigDecimal(0)) { index, acc, taxBracket ->
+                        val minIncome =
+                            taxType.taxBrackets.getOrNull(index - 1)?.maxIncome ?: BigDecimal(0)
+                        if (minIncome > incomeWithDeductions) {
+                            acc
+                        } else {
+                            val maxIncome =
+                                incomeWithDeductions.min(
+                                    taxBracket.maxIncome ?: incomeWithDeductions
+                                )
+                            acc + (maxIncome - minIncome) * taxBracket.rate
+                        }
                     }
                 }
-            }
-        }.setScale(2, RoundingMode.HALF_UP)
+            }.setScale(2, RoundingMode.HALF_UP)
+        }
     }
+
+    override suspend fun calculatePaycheckTax(
+        grossIncome: BigDecimal,
+        paidYearToDate: BigDecimal,
+        tax: Tax,
+        frequency: PayFrequency
+    ): BigDecimal {
+        val divisor = when (frequency) {
+            is PayFrequency.Monthly -> BigDecimal("12")
+            is PayFrequency.OneTime -> BigDecimal("1")
+            is PayFrequency.SemiMonthly -> BigDecimal("24")
+            is PayFrequency.SemiWeekly -> BigDecimal("26")
+            is PayFrequency.Weekly -> BigDecimal("52")
+        }
+
+        return withContext(defaultDispatcher) {
+            when (val taxType = tax.taxType) {
+                is Fixed -> calculateTax(grossIncome, tax) / divisor
+                is FixedWithMax -> {
+                    val maxTax = taxType.rate * taxType.maxTaxableIncome
+                    if (maxTax <= paidYearToDate) {
+                        BigDecimal("0")
+                    } else {
+                        val incomeWithDeductions =
+                            grossIncome - tax.deductions.fold(BigDecimal(0)) { acc, deduction ->
+                                acc + deduction.amount
+                            }
+                        val fullTax = incomeWithDeductions * taxType.rate
+                        minOf(fullTax, maxTax - paidYearToDate) / divisor
+                    }
+                }
+                is Progressive -> calculateTax(grossIncome, tax) / divisor
+            }.setScale(2, RoundingMode.HALF_UP)
+        }
+    }
+
 }
 
 @Module
--- app/src/test/java/com/sebastianvm/mango/data/CalculatorImplTest.kt
+++ app/src/test/java/com/sebastianvm/mango/data/CalculatorImplTest.kt
@@ -2,48 +2,108 @@ package com.sebastianvm.mango.data
 
 import com.google.common.truth.Truth.assertThat
 import com.sebastianvm.mango.BaseTest
+import com.sebastianvm.mango.model.PayFrequency
 import com.sebastianvm.mango.model.Taxes
+import com.sebastianvm.mango.util.date.DateRange
 import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.LocalDate
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
 
 class CalculatorImplTest : BaseTest() {
 
+    private val calculator = CalculatorImpl(dispatcher)
+
     @Test
     fun `calculate tax works`() = testScope.runTest {
         assertThat(
-            CalculatorImpl().calculateTax(
+            calculator.calculateTax(
                 BigDecimal("130000"),
                 Taxes.FederalIncome.taxEntity
             )
         ).isEqualToIgnoringScale(BigDecimal("21276"))
 
         assertThat(
-            CalculatorImpl().calculateTax(
+            calculator.calculateTax(
                 BigDecimal("130000"),
                 Taxes.Medicare.taxEntity
             )
         ).isEqualToIgnoringScale(BigDecimal("1885"))
 
         assertThat(
-            CalculatorImpl().calculateTax(
+            calculator.calculateTax(
                 BigDecimal("130000"),
                 Taxes.SocialSecurity.taxEntity
             )
         ).isEqualToIgnoringScale(BigDecimal("8060"))
 
         assertThat(
-            CalculatorImpl().calculateTax(
+            calculator.calculateTax(
                 BigDecimal("130000"),
                 Taxes.CaliforniaIncome.taxEntity
             )
         ).isEqualToIgnoringScale(BigDecimal("9195.66"))
 
         assertThat(
-            CalculatorImpl().calculateTax(
+            calculator.calculateTax(
                 BigDecimal("130000"),
                 Taxes.CaliforniaSdi.taxEntity
             )
         ).isEqualToIgnoringScale(BigDecimal("1170"))
+    }
+
+    @Test
+    fun `calculate paycheck tax works`() = testScope.runTest {
+        val frequency = PayFrequency.Weekly(
+            firstPayPeriod = DateRange(
+                LocalDate(2023, 1, 1),
+                LocalDate(2023, 1, 15)
+            ),
+            daysFromEndOfPayPeriodToPayDay = 0
+        )
+        assertThat(
+            calculator.calculatePaycheckTax(
+                grossIncome = BigDecimal("130000"),
+                paidYearToDate = BigDecimal("0"),
+                tax = Taxes.FederalIncome.taxEntity,
+                frequency = frequency
+            )
+        ).isEqualToIgnoringScale(BigDecimal("409.15"))
+
+        assertThat(
+            calculator.calculatePaycheckTax(
+                BigDecimal("130000"),
+                paidYearToDate = BigDecimal("0"),
+                Taxes.Medicare.taxEntity,
+                frequency = frequency
+            )
+        ).isEqualToIgnoringScale(BigDecimal("36.25"))
+
+        assertThat(
+            calculator.calculatePaycheckTax(
+                BigDecimal("130000"),
+                paidYearToDate = BigDecimal("0"),
+                Taxes.SocialSecurity.taxEntity,
+                frequency = frequency
+            )
+        ).isEqualToIgnoringScale(BigDecimal("155"))
+
+        assertThat(
+            calculator.calculatePaycheckTax(
+                BigDecimal("130000"),
+                paidYearToDate = BigDecimal("0"),
+                Taxes.CaliforniaIncome.taxEntity,
+                frequency = frequency
+            )
+        ).isEqualToIgnoringScale(BigDecimal("176.84"))
+
+        assertThat(
+            calculator.calculatePaycheckTax(
+                BigDecimal("130000"),
+                paidYearToDate = BigDecimal("0"),
+                Taxes.CaliforniaSdi.taxEntity,
+                frequency = frequency
+            )
+        ).isEqualToIgnoringScale(BigDecimal("22.5"))
     }
 }
